bdd7a9722028b97f889022889be449e37e276dcc
sparkty <christian.azcuna@gmail.com>
2026-07-17 14:44:37 +0800

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 python -m pip install --upgrade pip
-python -m pip install -r -requirements.txt
+python -m pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py ensure_superuser
